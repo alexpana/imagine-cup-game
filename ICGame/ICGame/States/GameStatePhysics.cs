aff5a2c18a2726b5242d3f90ccf7b7bb8f134262
Andrei Grigoriu <mudpedal@vertexarmy.org>
2013-01-17 16:09:20 +0200

    Added Physics demo state class, started branch

---
+++ ICGame/ICGame/States/GameStatePhysics.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using VertexArmy.Global;
+
+namespace VertexArmy.States
+{
+	internal class GameStatePhysics : IGameState
+	{
+		private ContentManager _contentManager;
+
+		public GameStatePhysics( ContentManager content )
+		{
+			_contentManager = content;
+		}
+
+		public void OnUpdate( GameTime dt )
+		{
+			
+		}
+
+		public void RenderScene()
+		{
+		
+		}
+
+		public void OnRender( GameTime dt )
+		{
+			
+		}
+
+		public void OnEnter()
+		{
+			
+		}
+
+		public void OnClose()
+		{
+			_contentManager.Unload( );
+		}
+	}
+}

223be398d51b6136f3119cc972176062c2bc0b83
timotei <timotei_cluj@yahoo.co.uk>
2013-01-23 10:54:31 +0200

    Add a small console app for generating the levels

---
+++ ICGame.LevelGenerator/Program.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.IO;
+
+namespace ICGame.LevelGenerator
+{
+	public class Program
+	{
+		public static void Main( string[] args )
+		{
+			string targetPath = args.Length > 0 ? args[0] : "../../../ICGame/ICGame/Content/Levels/";
+
+			Console.WriteLine( "Generating levels in " + targetPath );
+
+			GenerateLevels( targetPath );
+		}
+
+		private static void GenerateLevels( string targetPath )
+		{
+			GenerateTutorial( targetPath );
+		}
+
+		private static void GenerateTutorial( string targetPath )
+		{
+			string tutorialPath = targetPath + "tutorial.level";
+			using ( StreamWriter streamWriter = new StreamWriter( tutorialPath ) )
+			{
+			}
+		}
+	}
+}

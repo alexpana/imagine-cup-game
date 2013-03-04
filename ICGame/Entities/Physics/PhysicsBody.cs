28b928d7b77b974921185481484ca931f918f931
Andrei Grigoriu <mudpedal@vertexarmy.org>
2013-03-04 16:46:13 +0200

    Added PhysicsBody (attaches to ITransformable and Body and updates position/rotation

---
+++ ICGame/Entities/Physics/PhysicsBody.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using FarseerPhysics.Dynamics;
+using Microsoft.Xna.Framework;
+using VertexArmy.Graphics;
+using VertexArmy.Common;
+using VertexArmy.Utilities;
+
+namespace VertexArmy.Entities.Physics
+{
+	class BodyTransformableController : IUpdateableObject
+	{
+		private ITransformable _transformable;
+		public ITransformable Transformable
+		{
+			set
+			{
+				_transformable = value;
+				UpdateTransformableRotation( );
+				UpdateTransformablePosition( );
+			}
+			get { return _transformable; }
+		}
+		private Body _body;
+
+		private Vector2 _lastBodyPosition;
+		private float _lastBodyRotation;
+
+		private const float RotationError = 0.001f;
+		private const float PositionError = 0.001f;
+
+		public BodyTransformableController( Body body )
+		{
+			Transformable = null;
+			_body = body;
+
+			UpdateTransformableRotation( );
+			UpdateTransformablePosition( );
+		}
+
+		public BodyTransformableController(Body body, ITransformable transformable)
+		{
+			Transformable = transformable;
+			_body = body;
+
+			UpdateTransformableRotation( );
+			UpdateTransformablePosition( );
+		}
+
+		public void Update()
+		{
+			float rotationDelta = Math.Abs( _body.Rotation - _lastBodyRotation );
+			float positionDelta = (_body.Position - _lastBodyPosition).Length();
+
+			if ( rotationDelta > RotationError)
+			{
+				UpdateTransformableRotation();
+			}
+
+			if ( positionDelta > PositionError)
+			{
+				UpdateTransformablePosition( );
+			}
+		}
+
+		private void UpdateTransformableRotation()
+		{
+			_lastBodyRotation = _body.Rotation;
+			if (Transformable != null)
+			{
+				Transformable.SetRotation( new Quaternion(new Vector3(0f,0f,1f), _body.Rotation ) );
+			}
+		}
+
+		private void UpdateTransformablePosition()
+		{
+			_lastBodyPosition = _body.Position;
+			if (Transformable != null)
+			{
+				Transformable.SetPosition( new Vector3(UnitsConverter.ToDisplayUnits(_body.Position),0f) );
+			}
+		}
+	}
+}

f06278f73b97753d9115986b5111cc7f055a020c
Alexandru Pana <alex.pana@vertexarmy.org>
2013-03-23 10:00:05 +0200

    Perfected rotation in modelview

--- ICGame/Global/CursorManager.cs
+++ ICGame/Global/CursorManager.cs
@@ -95,9 +95,12 @@ namespace VertexArmy.Global
 
 		public void Render()
 		{
-			_tempSpriteBatch.Begin( SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.Default, RasterizerState.CullNone, null );
-			_tempSpriteBatch.Draw( _activeCursor.Sprite, new Rectangle( ( int ) _cursorPosition.X, ( int ) _cursorPosition.Y, 32, 32 ), Color.White );
-			_tempSpriteBatch.End( );
+			if ( _isVisible )
+			{
+				_tempSpriteBatch.Begin( SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.LinearClamp, DepthStencilState.Default, RasterizerState.CullNone, null );
+				_tempSpriteBatch.Draw( _activeCursor.Sprite, new Rectangle( ( int ) _cursorPosition.X, ( int ) _cursorPosition.Y, 32, 32 ), Color.White );
+				_tempSpriteBatch.End();
+			}
 		}
 
 		private bool IsCursorInsideWindow()

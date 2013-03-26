7abd1d0328f58e48a95bf4ae3821ba707710108e
timotei <timotei_cluj@yahoo.co.uk>
2013-03-26 23:18:20 +0200

    Add a preliminary menu cube

--- ICGame/States/Menu/GameStateMenu.cs
+++ ICGame/States/Menu/GameStateMenu.cs
@@ -5,8 +5,6 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-using VertexArmy.GameWorld.Prefabs;
-using VertexArmy.GameWorld.Prefabs.Structs;
 using VertexArmy.Global;
 using VertexArmy.Global.Managers;
 
@@ -14,8 +12,8 @@ namespace VertexArmy.States.Menu
 {
 	public class GameStateMenu : IGameState
 	{
-		private readonly MenuCube _mainMenuCube;
-		private readonly MenuCube _optionsMenuCube;
+		private MenuCube _mainMenuCube;
+		private MenuCube _optionsMenuCube;
 
 		private MenuCube _activeCube;
 
@@ -25,13 +23,79 @@ namespace VertexArmy.States.Menu
 
 		private readonly ContentManager _content;
 
-		private bool _shouldRotate = false;
-		private Quaternion _rotation = new Quaternion();
-
 		public GameStateMenu( ContentManager content )
 		{
 			_content = content;
+		}
 
+		private void ActivateMenuCube( MenuCube cube )
+		{
+			_activeCube = cube;
+			_activeCube.Spawn();
+		}
+
+		public void OnUpdate( GameTime gameTime )
+		{
+			if ( _activeCube != null )
+			{
+				if ( Platform.Instance.Input.IsKeyPressed( Keys.Left, false ) )
+				{
+					_activeCube.SelectPreviousItem();
+				}
+				else if ( Platform.Instance.Input.IsKeyPressed( Keys.Right, false ) )
+				{
+					_activeCube.SelectNextItem();
+				}
+				else if ( Platform.Instance.Input.IsKeyPressed( Keys.Enter, false ) )
+				{
+					_activeCube.Items[_activeCube.SelectedItem].Activate();
+				}
+				else if ( Platform.Instance.Input.IsKeyPressed( Keys.Escape, false ) ||
+						 Platform.Instance.Input.IsKeyPressed( Keys.Back, false ) )
+				{
+					_activeCube.Destroy();
+					_activeCube = _activeCube.PreviousMenu;
+				}
+
+				_activeCube.Update( gameTime );
+			}
+
+			Platform.Instance.PhysicsWorld.Step( Math.Min( ( float ) gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f, ( 1f / 30f ) ) );
+			FrameUpdateManager.Instance.Update( gameTime );
+		}
+
+		public void OnRender( GameTime gameTime )
+		{
+			if ( _activeCube != null )
+			{
+				_spriteBatch.Begin();
+
+				_spriteBatch.DrawString( _font, _activeCube.Title, new Vector2( 100, 100 ), Color.Black );
+
+				_spriteBatch.DrawString( _font, "< " + _activeCube.Items[_activeCube.SelectedItem].Title + " >",
+					new Vector2( 100, 150 ), Color.Black );
+
+				_spriteBatch.End();
+
+				SceneManager.Instance.Render( gameTime.ElapsedGameTime.Milliseconds );
+			}
+		}
+
+		public void OnEnter()
+		{
+			CreateMenus();
+
+			ActivateMenuCube( _mainMenuCube );
+			_spriteBatch = new SpriteBatch( Platform.Instance.Device );
+			_font = Platform.Instance.Content.Load<SpriteFont>( "fonts/SpriteFont1" );
+			_backgroundMusic = _content.Load<Song>( "music/proto1_menu" );
+
+			Platform.Instance.SoundPlayer.PlayMusic( _backgroundMusic );
+			GameWorldManager.Instance.SpawnEntity( "camera", "camera1", new Vector3( 0, 0, 100 ) );
+		}
+
+		private void CreateMenus()
+		{
 			_mainMenuCube = new MenuCube
 			{
 				Title = "Main menu",
@@ -59,100 +123,11 @@ namespace VertexArmy.States.Menu
 			};
 		}
 
-		private void ActivateMenuCube( MenuCube cube )
-		{
-			_activeCube = cube;
-		}
-
-		public void OnUpdate( GameTime gameTime )
-		{
-			if ( Platform.Instance.Input.IsKeyPressed( Keys.Left, false ) )
-			{
-				if ( _activeCube.SelectedItem == 0 )
-				{
-					_activeCube.SelectedItem = _activeCube.Items.Count - 1;
-				}
-				else
-				{
-					_activeCube.SelectedItem--;
-				}
-
-				_shouldRotate = true;
-			}
-			else if ( Platform.Instance.Input.IsKeyPressed( Keys.Right, false ) )
-			{
-				_activeCube.SelectedItem = ( _activeCube.SelectedItem + 1 ) % _activeCube.Items.Count;
-				_shouldRotate = true;
-			}
-			else if ( Platform.Instance.Input.IsKeyPressed( Keys.Enter, false ) )
-			{
-				_activeCube.Items[_activeCube.SelectedItem].Activate();
-			}
-			else if ( Platform.Instance.Input.IsKeyPressed( Keys.Escape, false ) ||
-					 Platform.Instance.Input.IsKeyPressed( Keys.Back, false ) )
-			{
-				if ( _activeCube.PreviousMenu != null )
-				{
-					ActivateMenuCube( _activeCube.PreviousMenu );
-				}
-			}
-
-			if ( _shouldRotate )
-			{
-				_rotation = Quaternion.Concatenate( _rotation, Quaternion.CreateFromAxisAngle( Vector3.UnitY, MathHelper.ToRadians( 10 ) ) );
-				_shouldRotate = false;
-			}
-
-			GameWorldManager.Instance.GetEntity( "mesh1" ).SetRotation( _rotation );
-
-			Platform.Instance.PhysicsWorld.Step( Math.Min( ( float ) gameTime.ElapsedGameTime.TotalMilliseconds * 0.001f, ( 1f / 30f ) ) );
-			FrameUpdateManager.Instance.Update( gameTime );
-		}
-
-		public void OnRender( GameTime gameTime )
-		{
-			if ( _activeCube != null )
-			{
-				_spriteBatch.Begin();
-
-				_spriteBatch.DrawString( _font, _activeCube.Title, new Vector2( 100, 100 ), Color.Black );
-
-				_spriteBatch.DrawString( _font, "< " + _activeCube.Items[_activeCube.SelectedItem].Title + " >",
-					new Vector2( 100, 150 ), Color.Black );
-
-				_spriteBatch.End();
-
-				SceneManager.Instance.Render( gameTime.ElapsedGameTime.Milliseconds );
-			}
-		}
-
-		public void OnEnter()
-		{
-			ActivateMenuCube( _mainMenuCube );
-			_spriteBatch = new SpriteBatch( Platform.Instance.Device );
-			_font = Platform.Instance.Content.Load<SpriteFont>( "fonts/SpriteFont1" );
-			_backgroundMusic = _content.Load<Song>( "music/proto1_menu" );
-
-			Platform.Instance.SoundPlayer.PlayMusic( _backgroundMusic );
-
-			PrefabEntity mesh = new PrefabEntity();
-
-			GameWorldManager.Instance.SpawnEntity( "camera", "camera1", new Vector3( 0, 0, 100 ) );
-
-			MeshSceneNodePrefab crateSceneNode = new MeshSceneNodePrefab
-			{
-				Material = "CelShadingMaterial",
-				Mesh = "models/menu_cube",
-				Name = "Mesh",
-				LocalRotation = new Quaternion( new Vector3( 0f, 0f, 0f ), 0f )
-			};
-
-			mesh.RegisterMeshSceneNode( crateSceneNode );
-			GameWorldManager.Instance.SpawnEntity( mesh, "mesh1", new Vector3( 0f, 0, 0f ) );
-		}
-
 		public void OnClose()
 		{
+			_mainMenuCube.Destroy();
+			_optionsMenuCube.Destroy();
+
 			SceneManager.Instance.Clear();
 			GameWorldManager.Instance.Clear();
 			Platform.Instance.SoundPlayer.StopMusic();
--- ICGame/States/Menu/MenuCube.cs
+++ ICGame/States/Menu/MenuCube.cs
@@ -1,16 +1,123 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using VertexArmy.GameWorld.Prefabs;
+using VertexArmy.GameWorld.Prefabs.Structs;
+using VertexArmy.Global.Managers;
 
 namespace VertexArmy.States.Menu
 {
 	public class MenuCube
 	{
+		// remove this?
 		public MenuCube PreviousMenu { get; set; }
+
 		public List<MenuItem> Items { get; set; }
 		public int SelectedItem { get; set; }
 
 		public string Title { get; set; }
 
-		public Vector2 Position { get; set; }
+		public string Id { get; private set; }
+		public PrefabEntity Mesh { get; private set; }
+
+		private bool _requestedRotation;
+		private bool _isRotating;
+		private float _currentRotationTime;
+		private const float RotationTime = 200.0f;
+		private const float RotationStep = 90.0f;
+		private Quaternion _previousRotation = Quaternion.Identity;
+		private Quaternion _nextRotation = Quaternion.Identity;
+		private Quaternion _rotation = Quaternion.Identity;
+
+		public MenuCube()
+		{
+			Id = Guid.NewGuid().ToString();
+			InitMesh();
+		}
+
+		public void Spawn()
+		{
+			GameWorldManager.Instance.SpawnEntity( Mesh, Id, new Vector3( 0f, 0, 0f ) );
+		}
+
+		public void Destroy()
+		{
+			GameWorldManager.Instance.RemoveEntity( Id );
+		}
+
+		public void Update( GameTime gameTime )
+		{
+			if ( _requestedRotation )
+			{
+				_currentRotationTime = MathHelper.Clamp( _currentRotationTime + ( float ) gameTime.ElapsedGameTime.TotalMilliseconds, 0, RotationTime );
+				_rotation = Quaternion.Lerp( _previousRotation, _nextRotation, _currentRotationTime / RotationTime );
+
+				if ( _currentRotationTime >= RotationTime )
+				{
+					_requestedRotation = false;
+					_isRotating = false;
+				}
+			}
+
+			GameWorldManager.Instance.GetEntity( Id ).SetRotation( _rotation );
+		}
+
+		public void SelectPreviousItem()
+		{
+			if ( _isRotating )
+			{
+				return;
+			}
+
+			if ( SelectedItem == 0 )
+			{
+				SelectedItem = Items.Count - 1;
+			}
+			else
+			{
+				SelectedItem--;
+			}
+
+			InitRotation( -RotationStep );
+		}
+
+		public void SelectNextItem()
+		{
+			if ( _isRotating )
+			{
+				return;
+			}
+
+			SelectedItem = ( SelectedItem + 1 ) % Items.Count;
+			InitRotation( RotationStep );
+		}
+
+		private void InitMesh()
+		{
+			Mesh = new PrefabEntity();
+
+			MeshSceneNodePrefab crateSceneNode = new MeshSceneNodePrefab
+			{
+				Material = "CelShadingMaterial",
+				Mesh = "models/menu_cube",
+				Name = Id + "Mesh",
+				LocalRotation = new Quaternion( new Vector3( 0f, 0f, 0f ), 0f )
+			};
+
+			Mesh.RegisterMeshSceneNode( crateSceneNode );
+		}
+
+		private void InitRotation( float angle )
+		{
+			_requestedRotation = true;
+			if ( !_isRotating )
+			{
+				_previousRotation = _rotation;
+				_nextRotation = Quaternion.Concatenate( _rotation, Quaternion.CreateFromAxisAngle( Vector3.UnitY, MathHelper.ToRadians( angle ) ) );
+				_currentRotationTime = 0;
+
+				_isRotating = true;
+			}
+		}
 	}
 }

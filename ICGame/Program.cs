d54c37f7f8fdb3cd8a1c13c4ab368320bd08a9f4
timotei <timotei_cluj@yahoo.co.uk>
2013-03-29 22:19:05 +0200

    Show a message box when an exception arises, instead of closing silently the game

--- ICGame/Program.cs
+++ ICGame/Program.cs
@@ -1,15 +1,26 @@
 using System;
+using System.Runtime.InteropServices;
 
 namespace VertexArmy
 {
 #if WINDOWS || XBOX
 	internal static class Program
 	{
+		[DllImport( "user32.dll", CharSet = CharSet.Auto )]
+		public static extern uint MessageBox( IntPtr hWnd, String text, String caption, uint type );
+
 		[STAThread]
 		private static void Main()
 		{
+			AppDomain.CurrentDomain.UnhandledException += CurrentDomainOnUnhandledException;
+
 			MainGame game = new MainGame();
 			game.Run();
+		}
+
+		private static void CurrentDomainOnUnhandledException( object sender, UnhandledExceptionEventArgs unhandledExceptionEventArgs )
+		{
+			MessageBox( IntPtr.Zero, unhandledExceptionEventArgs.ExceptionObject == null ? "" : unhandledExceptionEventArgs.ExceptionObject.ToString(), "Exception", 0x00000010 );
 		}
 	}
 #endif

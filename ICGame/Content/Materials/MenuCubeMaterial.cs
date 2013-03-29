5c98c17b331c4f3498c59b28772d8ad903d6e854
timotei <timotei_cluj@yahoo.co.uk>
2013-03-29 20:06:05 +0200

    Add the 'MenuCubeMaterial' class

---
+++ ICGame/Content/Materials/MenuCubeMaterial.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using VertexArmy.Global;
+using VertexArmy.Graphics;
+
+namespace VertexArmy.Content.Materials
+{
+	class MenuCubeMaterial
+	{
+		public static Material CreateMaterial()
+		{
+			Material material = new Material();
+			Effect effect = Platform.Instance.Content.Load<Effect>( @"effects\MenuCube" );
+			material.Effect = effect;
+
+			material.AddParameter( "ColorMap", Platform.Instance.Content.Load<Texture2D>( "images/" + "color" ) );
+			material.AddParameter( "NormalMap", Platform.Instance.Content.Load<Texture2D>( "images/" + "normal" ) );
+			material.AddParameter( "SpecularMap", Platform.Instance.Content.Load<Texture2D>( "images/" + "specular" ) );
+			material.AddParameter( "AOMap", Platform.Instance.Content.Load<Texture2D>( "images/" + "ao" ) );
+			material.AddParameter( "matWorldViewProj", Matrix.Identity );
+			material.AddParameter( "matWorldInverseTranspose", Matrix.Identity );
+			material.AddParameter( "matWorld", Matrix.Identity );
+			material.AddParameter( "eyePosition", Vector3.Zero );
+			material.AddParameter( "lightPosition", Vector3.Zero );
+
+			return material;
+		}
+	}
+}

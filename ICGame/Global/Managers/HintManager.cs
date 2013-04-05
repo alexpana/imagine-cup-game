68c7f16de2d85375a6c1e23c5bcb4a5e772ccb52
Andrei Grigoriu <mudpedal@vertexarmy.org>
2013-04-05 08:42:25 +0300

    Adding HintManager

---
+++ ICGame/Global/Managers/HintManager.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+
+namespace VertexArmy.Global.Managers
+{
+	public class HintManager
+	{
+		private List<Hint> _activeHints;
+
+		public HintManager()
+		{
+			_activeHints = new List<Hint>();
+		}
+
+		public void SpawnHint( string text, Vector2 position, float time )
+		{
+			_activeHints.Add( new Hint() { Text = text, Position = position, Time = time } );
+		}
+		public static HintManager Instance
+		{
+			get { return HintManagerInstanceHolder.Instance; }
+		}
+
+		private static class HintManagerInstanceHolder
+		{
+			// ReSharper disable MemberHidesStaticFromOuterClass
+			public static readonly HintManager Instance = new HintManager();
+			// ReSharper restore MemberHidesStaticFromOuterClass
+		}
+	}
+
+	public class Hint
+	{
+		public string Text;
+		public Vector2 Position;
+		public float Time; // in seconds
+		public float RegisteredTime;
+	}
+
+}

154dd57db26d7d57bc66031a7b8e678f8a1c8016
Andrei Grigoriu <mudpedal@vertexarmy.org>
2013-04-09 19:59:08 +0300

    Added lvl prefabs and serialization

--- ICGame/Global/Managers/PrefabRepository.cs
+++ ICGame/Global/Managers/PrefabRepository.cs
@@ -1,15 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using Microsoft.Xna.Framework;
+using VertexArmy.Content.Prefabs;
 using VertexArmy.GameWorld.Prefabs;
+using VertexArmy.Serialization;
 
 namespace VertexArmy.Global.Managers
 {
 	public class PrefabRepository
 	{
 		private Dictionary<string, PrefabEntity> _prefabs;
+		private Dictionary<string, LevelPrefab> _levelPrefabs;
 
 		public void RegisterPrefab( string name, PrefabEntity prefab )
 		{
 			_prefabs.Add( name, prefab );
+			prefab.Name = name;
 		}
 
 		public void UnregisterPrefab( string name )
@@ -25,6 +31,45 @@ namespace VertexArmy.Global.Managers
 			return _prefabs[name];
 		}
 
+		public void UnloadLevelPrefab( string filepath )
+		{
+			if ( _levelPrefabs.ContainsKey( filepath ) )
+			{
+				_levelPrefabs.Remove( filepath );
+			}
+		}
+
+		public LevelPrefab GetLevelPrefab( string filepath )
+		{
+			if ( !_levelPrefabs.ContainsKey( filepath ) )
+			{
+				if ( File.Exists( filepath ) )
+				{
+					Stream stream = TitleContainer.OpenStream( filepath );
+
+					ISerializer<LevelPrefab> serializer = SerializerFactory.CreateSerializer<LevelPrefab>();
+					LevelPrefab level = serializer.ReadObject( stream );
+					stream.Close();
+					_levelPrefabs.Add( filepath, level );
+				}
+				else
+				{
+					_levelPrefabs.Add( filepath, new LevelPrefab() { filepath = filepath } );
+				}
+			}
+
+			return _levelPrefabs[filepath];
+		}
+
+
+		public void SaveLevelPrefab( string filepath )
+		{
+			if ( _levelPrefabs.ContainsKey( filepath ) )
+			{
+				_levelPrefabs[filepath].SerializeLevel();
+			}
+		}
+
 		public static PrefabRepository Instance
 		{
 			get { return PrefabRepositoryInstanceHolder.Instance; }
@@ -33,6 +78,7 @@ namespace VertexArmy.Global.Managers
 		public PrefabRepository()
 		{
 			_prefabs = new Dictionary<string, PrefabEntity>();
+			_levelPrefabs = new Dictionary<string, LevelPrefab>();
 		}
 
 		private static class PrefabRepositoryInstanceHolder
--- ICGame/States/GameStateTutorial.cs
+++ ICGame/States/GameStateTutorial.cs
@@ -303,7 +303,7 @@ namespace VertexArmy.States
 		public void LoadDynamics()
 		{
 			GameWorldManager.Instance.SpawnEntity( "Camera", "camera1", new Vector3( 0, -200, 800 ) );
-			GameWorldManager.Instance.SpawnEntity( "Robot", "robotPlayer", new Vector3( -150f, 100f, 0f ), new Vector3( 1f, 1f, 1f ) );
+			GameWorldManager.Instance.SpawnEntity( "Robot", "robotPlayer", new Vector3( -150f, 100f, 0f ), new Vector3( 1.5f, 1.5f, 2.5f ) );
 			GameWorldManager.Instance.SpawnEntity( "WallBackground", "wallBackground1",
 				new Vector3( 0f, 0f, -800f ), Quaternion.CreateFromAxisAngle( Vector3.UnitX, -0.3f ), 150 );
 
@@ -391,13 +391,14 @@ namespace VertexArmy.States
 			LoadDynamics();
 			LoadTriggers();
 
-			GameWorldManager.Instance.SaveState();
+
 		}
 
 		public override void OnEnter()
 		{
 
 			LoadLevel();
+			GameWorldManager.Instance.SaveState();
 			Platform.Instance.PhysicsWorld.Gravity = Vector2.UnitY * Platform.Instance.PhysicsWorld.Gravity.Length();
 
 			FrameUpdateManager.Instance.Register( HintManager.Instance );

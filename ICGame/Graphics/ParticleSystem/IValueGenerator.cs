b6fef9a1361727d37fcc37729f28f8295cbaaa36
Alexandru Pana <alex.pana@vertexarmy.org>
2013-04-09 15:26:50 +0300

    Added Particle System

---
+++ ICGame/Graphics/ParticleSystem/IValueGenerator.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace VertexArmy.Graphics.ParticleSystem
+{
+	interface IValueGenerator
+	{
+		Vector3 GetVector3Value( float index );
+		float GetFloatValue( float index );
+	}
+}
---
+++ ICGame/Graphics/ParticleSystem/Particle.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace VertexArmy.Graphics.ParticleSystem
+{
+	class Particle
+	{
+		public ParticleEmitter Parent;
+		public Vector3 Position { get; set; }
+		public float Lifetime { get; set; }
+		public float Age { get; set; }
+		public Vector3 Direction { get; set; }
+		public float Speed { get; set; }
+		public float Rotation { get; set; }
+		public float Scale { get; set; }
+		public float Alpha { get; set; }
+		public Material RenderMaterial { get; set; }
+	}
+}

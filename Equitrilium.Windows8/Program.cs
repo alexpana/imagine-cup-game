373d42c9c42e4ba188fb631bb131d7004c63850a
timotei <timotei_cluj@yahoo.co.uk>
2013-04-13 18:38:24 +0300

    Add initial Win8 projects

---
+++ Equitrilium.Windows8/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using TestMono;
+
+namespace Equitrilium.Windows8
+{
+	/// <summary>
+	/// The main class.
+	/// </summary>
+	public static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		static void Main()
+		{
+			var factory = new MonoGame.Framework.GameFrameworkViewSource<MainGame>();
+			Windows.ApplicationModel.Core.CoreApplication.Run( factory );
+		}
+	}
+}

b405cee21d67a07ccee52cad97d8607e7020a4a9
timotei <timotei_cluj@yahoo.co.uk>
2013-04-14 15:33:14 +0300

    Fix the SAF and DOF materials

--- ICGame/Content/Materials/DepthOfFieldMaterial.cs
+++ ICGame/Content/Materials/DepthOfFieldMaterial.cs
@@ -17,9 +17,9 @@ namespace VertexArmy.Content.Materials
 			mat.AddParameter( "ColorMap", null );
 			mat.AddParameter( "DepthMap", null );
 			mat.AddParameter( "matWorldViewProj", Matrix.Identity );
-			mat.AddParameter( "distance", 800 );
-			mat.AddParameter( "no_dof_range", 300 );
-			mat.AddParameter( "dof_range", 500 );
+			mat.AddParameter( "distance", 800.0f );
+			mat.AddParameter( "no_dof_range", 300.0f );
+			mat.AddParameter( "dof_range", 500.0f );
 			mat.AddParameter( "near", 1f );
 			mat.AddParameter( "far", 10000f );
 			mat.AddParameter( "blurDistance", 0.0023f );

bb7cf3bde48429d51aeae8bf24648a76c063922c
adi <space.folding@yahoo.com.au>
2013-04-14 17:10:33 +0300

    filled some of the shape trigger classes code

---
+++ ICGame/Global/ShapeListeners/HintShapeListener.cs
@@ -0,0 +1,28 @@
+﻿using VertexArmy.Global.Behaviours;
+
+
+namespace VertexArmy.Global.ShapeListeners
+{
+	class HintShapeListener : IShapeListener
+	{
+		public HintShapeListener ()
+		{
+			
+		}
+
+		public void OnEnterShape()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void OnExitShape()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void OnEachFrameInsideShape()
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}

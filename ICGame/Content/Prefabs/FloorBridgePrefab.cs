e9b9c2440c7fcd1fca28f1bb5d7efce27d5b4e7c
Andrei Grigoriu <mudpedal@vertexarmy.org>
2013-04-16 01:23:23 +0300

    Fixed lvl2

--- ICGame/Content/Prefabs/FloorBridgePrefab.cs
+++ ICGame/Content/Prefabs/FloorBridgePrefab.cs
@@ -22,7 +22,7 @@ namespace VertexArmy.Content.Prefabs
 
 			BodyPrefab floorBody = new BodyPrefab
 			{
-				Friction = 1f,
+				Friction = 0.3f,
 				LocalPosition = Vector2.Zero,
 				Name = "FloorBody",
 				Shapes = new List<ShapePrefab> { floorShape },
